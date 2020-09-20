6ef375a3441c8596f9bedd1c58d7827c3db713bf
JungSoo Lee <love910321@naver.com>
2020-09-20 20:37:15 +0900

    add onnx code, optimize softer_softmax

--- Knowledge Distillation Cifar-10.py
+++ Knowledge Distillation Cifar-10.py
@@ -1,7 +1,7 @@
-from tensorflow.keras.datasets import cifar10, cifar100
+from tensorflow.keras.datasets import cifar10, cifar100, mnist,fashion_mnist
 from tensorflow.keras.models import Sequential
 import tensorflow.keras.backend as K
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation,GlobalAveragePooling2D
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.layers import BatchNormalization
 import numpy as np
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras import activations
+import time
+import onnx
+import keras2onnx
 
 np.random.seed(0)
 def softer_softmax(x, axis=-1):
@@ -26,43 +29,51 @@ def softer_softmax(x, axis=-1):
         raise ValueError('Cannot apply softmax to a tensor that is 1D. '
                          'Received input: %s' % x)
 
-        
-# softmax 결과물을 softer_softmax로 변환
+
 def softmax2softer_softmax(x):
     y = []
 
     for i in range(len(x)):
         distilled_x = []  # x/T = 분자
+        distilled_xAppend = distilled_x.append
         for j in range(len(x[0])):
-            distilled_x.append(np.log((x[i][j]) * sum(x[i]))/T)  # 적용된 소프트맥스를 역연산하여 원래의 확률을 구하고, T 로 나누어 정규화
+            distilled_xAppend(np.log((x[i][j]) * sum(x[i]))/T)  # 적용된 소프트맥스를 역연산하여 원래의 확률을 구하고, T 로 나누어 정규화
 
         softer_x = []  # softer_softmax가 적용된 x
+        softer_xAppend = softer_x.append
         for j in range(len(x[0])):
-            softer_x.append(np.exp(distilled_x[j])/sum(np.exp(distilled_x)))
+            softer_xAppend(np.exp(distilled_x[j])/sum(np.exp(distilled_x)))
 
-        y.append(softer_x) 
+        y.append(softer_x)  # 분자.분모
     y = np.array(y)
     return y
 
 
 dataset = cifar100
-img_rows = 32
-img_cols = 32
-channels = 3
-T = 10.0  # T-value
+dataset_name = "cifar100"
+(x_train, y_train), (x_test, y_test) = dataset.load_data()
+img_rows = x_train.shape[1]
+img_cols = x_train.shape[2]
+if len(x_train.shape) == 4:
+    img_channels = x_train.shape[3]
+else:
+    img_channels = 1
 
+input_shape = (img_rows, img_cols, img_channels)
+T = 10.0  # T-value
+batch_size = 512
+epoch = 300
+dropout_rate = 0.4
+teacher_dense = 512
+student_dense = 512
+num_classes = len(np.unique(y_train))
 result_acc = []
 result_loss = []
-input_shape = (img_rows, img_cols, channels)
+result_time = []
 earlystopping = EarlyStopping(monitor="val_loss", patience=10)
-batch_size = 512
-epoch = 50
 
-(x_train, y_train), (x_test, y_test) = dataset.load_data()
-num_classes = len(np.unique(y_train))
-
-x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, channels)
-x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, channels)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, img_channels)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, img_channels)
 
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
@@ -78,25 +89,25 @@ def teacher_model():
     model.add(Conv2D(64, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout_rate))
 
     model.add(Conv2D(128, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(Conv2D(128, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout_rate))
 
     model.add(Conv2D(256, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(Conv2D(256, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout_rate))
 
     model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Dropout(0.3))
+    model.add(Dense(teacher_dense))
+    model.add(Dropout(dropout_rate))
     model.add(Activation("relu"))
     model.add(Dense(num_classes, activation="softmax"))
 
@@ -104,24 +115,28 @@ def teacher_model():
 
     hist = model.fit(x_train, y_train,
                      batch_size=batch_size,
-                     epochs=epoch+50,
+                     epochs=epoch,
                      verbose=2,
                      validation_data=(x_test, y_test),
                      callbacks=[earlystopping])
-
-    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
+    start = time.time()
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=1)
+    finish = time.time() - start
+    result_time.append(finish)
     result_acc.append(test_acc)
     result_loss.append(test_loss)
 
+    model.summary()
     print('teacher Test loss:', test_loss)
     print('teacher Test accuracy:', test_acc)
 
 
-    model.save("teacher.h5")
+    model.save("teacher-"+dataset_name+".h5")
     return model, hist
 
 # distilled model
 def student_model1(teacher_model):
+    print("student1 training start1")
     model = Sequential()
 
     model.add(Conv2D(64, (3, 3), padding="same", input_shape=input_shape))
@@ -129,18 +144,18 @@ def student_model1(teacher_model):
     model.add(Conv2D(64, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout_rate))
 
     model.add(Conv2D(128, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(Conv2D(128, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout_rate))
 
     model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Dropout(0.3))
+    model.add(Dense(student_dense))
+    model.add(Dropout(dropout_rate))
     model.add(Activation("relu"))
     model.add(Dense(num_classes, activation=softer_softmax))
 
@@ -160,24 +175,29 @@ def student_model1(teacher_model):
                       validation_data=(x_test, soft_test_labels),
                       callbacks=[earlystopping])
 
-    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
+    start = time.time()
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=1)
+    finish = time.time() - start
+    result_time.append(finish)
     result_acc.append(test_acc)
     result_loss.append(test_loss)
+    model.summary()
     print('student1 softer Test loss:', test_loss)
     print('student1 softer Test accuracy:', test_acc)
-    print(model.predict(x_test))
 
     model.layers[-1].activation = activations.softmax  # 활성화 함수 교체
-    model.save("test.h5")  # save, load로 교체한 활성화 함수 적용
-    model = load_model("test.h5")
-    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
+    model.save("student1-"+dataset_name+".h5")
+    model = load_model("student1-"+dataset_name+".h5")
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=1)
     result_acc.append(test_acc)
     result_loss.append(test_loss)
     print('student1 Test loss:', test_loss)
     print('student1 Test accuracy:', test_acc)
-    print(model.predict(x_test))
 
-    model.save("student1.h5")
+    model.save("student1-"+dataset_name+".h5")
+    onnx_model = keras2onnx.convert_keras(model, model.name)
+    onnx.save_model(onnx_model, "student1-"+dataset_name+".onnx")
+    print("onnx saved")
     return hist1
 
 
@@ -189,18 +209,18 @@ def student_model2():
     model.add(Conv2D(64, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout_rate))
 
     model.add(Conv2D(128, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(Conv2D(128, (3, 3), padding="same"))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout_rate))
 
     model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Dropout(0.3))
+    model.add(Dense(student_dense))
+    model.add(Dropout(dropout_rate))
     model.add(Activation("relu"))
     model.add(Dense(num_classes, activation="softmax"))
 
@@ -215,14 +235,18 @@ def student_model2():
                       validation_data=(x_test, y_test),
                       callbacks=[earlystopping])
 
-    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
+    start = time.time()
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=1)
+    finish = time.time() - start
+    result_time.append(finish)
     result_acc.append(test_acc)
     result_loss.append(test_loss)
 
+    model.summary()
     print('student2 Test loss:', test_loss)
     print('student2 Test accuracy:', test_acc)
 
-    model.save("student2.h5")
+    model.save("student2-"+dataset_name+".h5")
     return hist1
 
 
@@ -232,10 +256,10 @@ hist1 = student_model1(teacher)  # improved
 
 hist2 = student_model2()
 
-# 교사 모델, 증류받은 모델의 softer_softmax, 증류받은 모델의 softmax, 일반 모델
+print("교사, 증류된 학생 softermax, 증류된 학생 softmax, 일반모델")
 print(result_acc)
 print(result_loss)
-
+print(result_time)
 plt.plot(teacher_hist.history['accuracy'])
 plt.plot(hist1.history['accuracy'])
 plt.plot(hist2.history['accuracy'])
@@ -243,7 +267,7 @@ plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['teacher', 'student1', "student2"], loc='upper left')
-plt.show()
+#plt.show()
 
 plt.plot(teacher_hist.history['val_accuracy'])
 plt.plot(hist1.history['val_accuracy'])
@@ -252,4 +276,22 @@ plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['teacher', 'student1', "student2"], loc='upper left')
-plt.show()
+#plt.show()
+
+plt.plot(teacher_hist.history['loss'])
+plt.plot(hist1.history['loss'])
+plt.plot(hist2.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['teacher', 'student1', "student2"], loc='upper left')
+#plt.show()
+
+plt.plot(teacher_hist.history['val_loss'])
+plt.plot(hist1.history['val_loss'])
+plt.plot(hist2.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['teacher', 'student1', "student2"], loc='upper left')
+#plt.show()
